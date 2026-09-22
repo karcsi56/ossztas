7c9a9ca255f02a3ab82786140ebd20bd9b6a5c76
tanulo <kkocsis106@gmail.com>
2026-09-22 10:04:26 +0200

    a 15 feladat megoldva

---
+++ szamtanikozep/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamtanikozep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg az első számot ");
+            int szam1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Add meg az második számot ");
+            int szam2 = int.Parse(Console.ReadLine());
+            double szamtanikozep = ((double)szam1+szam2)/2;
+
+            Console.WriteLine($"A számok számtani közepe: {szamtanikozep}");
+            Console.ReadLine();
+        }
+    }
+}
